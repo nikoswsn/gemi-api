abb43cfaaf53f34fef231b6e81b0dee2a31a3f22
nikoswsn <n.lykotomaros@pivopt.com>
2024-11-01 17:48:48 +0200

    services integration and init

--- src/api.ts
+++ src/api.ts
@@ -4,16 +4,26 @@ import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
+let apiKey = process.env.GEMH_API_KEY
+
 const api: AxiosInstance = axios.create({
   baseURL: 'https://opendata-api.businessportal.gr/api/opendata/v1', 
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'api_key': process.env.GEMH_API_KEY,
     },
 });
+// Interceptor to add the api_key to the headers dynamically
+api.interceptors.request.use((config) => {
+  if (apiKey) {
+    config.headers['api_key'] = apiKey;
+  }
+  return config;
+});
 
-
+export const init = (api_key:string) => {
+  apiKey=api_key
+}
 
 
 export default api;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import { init } from "./api";
 import companiesService from "./api/companies.api";
+import downloadFilesService from "./api/downloadFiles.api";
+import healthService from "./api/health.api";
+import metadataService from "./api/metadata.api";
 
 const gemi = {
-    companies: companiesService
+    companies: companiesService,
+    metadata: metadataService,
+    files: downloadFilesService,
+    health: healthService,
+    init
   };
 
 export default gemi;
